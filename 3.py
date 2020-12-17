17f661ff9c0434a6b427c66e2470ba8c2a06ac90
Muhammad Fikri Wibowo <45762569+muhfikriw@users.noreply.github.com>
2020-12-17 23:01:09 +0700

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,9 +1,10 @@
+import math
 n = int(input("berapa n ? "))
 def bintang(n):
     try:
         if n > 0:
             if (n%2 != 0):
-                n = round(n/2)
+                n = math.ceil(n/2)
                 for i in range(n):
                     a = "*" + "**"*i
                     print(a.center(20," "))
@@ -15,7 +16,6 @@ def bintang(n):
                     print(a.center(20," "))
             else:
                 print("angkamu negatif \natau \nbukan bil ganjil")
-
     except ValueError:
         print("bukan angka")
 
