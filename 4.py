bad15f3332f2c31f9080375002eb3a2334d42359
Muhammad Fikri Wibowo <45762569+muhfikriw@users.noreply.github.com>
2020-12-18 23:10:08 +0700

    Update 4.py

--- 4.py
+++ 4.py
@@ -3,7 +3,7 @@ import random
 def sufflestring(k,x):
     text = list(k)
     lis = []
-    for i in range(x+1):
+    for i in range(x):
         random.shuffle(text)
         a = ("".join(text))
         if a not in lis:
